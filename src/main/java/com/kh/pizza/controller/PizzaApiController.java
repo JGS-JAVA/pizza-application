9ed3f01d42f2729963b49fb2627f626e03edc2f0
JGS-JAVA <wlrncom@naver.com>
2025-01-28 10:33:53 +0900

    피자 템플릿 추가

---
+++ src/main/java/com/kh/pizza/controller/PizzaApiController.java
@@ -0,0 +1,19 @@
+package com.kh.pizza.controller;
+
+
+public class PizzaApiController {
+
+
+
+    @GetMapping("/pizzas")
+    public getAllPizzas() {
+
+    }
+
+    @PostMapping("/pizzas")
+    public  createPizza() {
+
+        return ;
+    }
+}
+
