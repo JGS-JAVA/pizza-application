065b0fc28a1c7a6ca10247e9514ff85d8ae0f0c1
JGS-JAVA <wlrncom@naver.com>
2025-01-28 10:16:19 +0900

    피자 템플릿 추가

---
+++ src/test/java/com/kh/pizza/PizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
